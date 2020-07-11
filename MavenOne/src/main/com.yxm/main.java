740f3547156527e211ece563163a2de0d528cf48
liuzhengquan <1360568420@qq.com>
2020-07-11 15:31:17 +0800

    提交新工程3

--- MavenOne/src/main/com.yxm/main.java
+++ MavenOne/src/main/com.yxm/main.java
@@ -1,3 +1,11 @@
-public class main {
+import java.util.Arrays;
 
+public class main {
+    public static void main(String[] args) {
+        System.out.println("args = ");
+        System.out.println("args = ");
+        System.out.println("args = ");
+        System.out.println("args = ");
+        System.out.println("args = ");
+    }
 }
